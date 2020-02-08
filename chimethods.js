1aa753b46702355138ee65f5e2903b66d63241ff
Eldaste <oliver.m.wimmer@vanderbilt.edu>
2020-02-07 19:38:14 -0600

    ...

--- chimethods.js
+++ chimethods.js
@@ -191,7 +191,7 @@ exports.loadTable = function (table, client){
 					
 				// Add user IDs back into tyhe Queue
 				for(let y=0;y<tablebase[x].queued.length;y++){
-					let user=compUser(table[x].queued[y], sx);
+					let user=compUser(tablebase[x].queued[y], sx);
 					table[sx].queued.push(user);
 					
 					sx.send(user);
